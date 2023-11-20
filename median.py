5d661c30a256b87176b4fad9f70315d24cc5e992
ErhanTK <125463792+ErhanTK@users.noreply.github.com>
2023-11-20 18:30:58 +0000

    Update median.py

--- median.py
+++ median.py
@@ -10,3 +10,14 @@ while True:
     else:
         break
 print(numbers)
+
+if len(numbers) % 2 != 0:
+    midl_sorted = sorted(numbers)
+    midl_index = (len(midl_sorted)) // 2
+    median = midl_sorted[midl_index]
+else:
+    sorted_numbers = sorted(numbers)
+    mid_index1, mid_index2 = len(sorted_numbers) // 2 - 1, len(sorted_numbers) // 2
+    median = (sorted_numbers[mid_index1] + sorted_numbers[mid_index2]) / 2  # Average of two middle elements
+
+print("Median:", median)
